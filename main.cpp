7e0618b433b01871e4cd6ff8381cea599c13945c
Gabriel Cristian <gabrielcristianms@gmail.com>
2022-05-27 04:51:16 -0300

    feat: criação dos esqueletos das funções de manipulação de conta

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include "conta.h"
 
+Conta createAccount(const int acc_number){
+    Conta conta(acc_number);
+    return conta;
+}
+
+double checkBalance(int acc_number){
+    return 0.0;
+}
+
+bool credit(int acc_number, double value){
+    ;
+}
+
+bool debit(int acc_number, double value){
+    ;
+}
+
+bool transfer(int origin_acc, int destination_acc, double value){
+    ;
+}
+
+
 int main(){
     //iniciar programa
+        //Inicializar vector de contas
         //listar opções
             //cadastrar conta
             //consultar conta
