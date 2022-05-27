e4fea91eec5f172d86cac828e1b324aed67cee29
Gabriel Cristian <gabrielcristianms@gmail.com>
2022-05-27 20:13:42 -0300

    feat: operações e interface implementadas

--- main.cpp
+++ main.cpp
@@ -2,28 +2,6 @@
 #include <string>
 #include "conta.h"
 
-Conta createAccount(const int acc_number){
-    Conta conta(acc_number);
-    return conta;
-}
-
-double checkBalance(int acc_number){
-    return 0.0;
-}
-
-bool credit(int acc_number, double value){
-    return false;
-}
-
-bool debit(int acc_number, double value){
-    return false;
-}
-
-bool transfer(int origin_acc, int destination_acc, double value){
-    return false;
-}
-
-
 int main(){
     //iniciar programa
     std::vector<Conta> listaContas;
@@ -43,42 +21,50 @@ int main(){
             std::cout << "Voce escolheu 'Cadastrar uma conta'! Por favor, insira o numero de conta que deseja para a nova conta:\n";
             std::cin >> input;
             ss << input;
-            int numeroConta;
-            ss >> numeroConta;
+            int numero_conta = 0;
+            ss >> numero_conta;
             ss.clear();
             input.clear();
-            Conta novaConta = createAccount(numeroConta);
+            Conta novaConta = Conta(numero_conta);
             listaContas.push_back(novaConta);
-            std::cout << "A conta " << novaConta.getAccountNumber() << " foi criada com sucesso!\n\n";
+            std::cout << ">>>>>>> A conta " << novaConta.getAccountNumber() << " foi criada com sucesso!\n\n";
         }
         //Consultar Conta
         else if(escolha == '2'){
             std::cout << "Por favor, digite o numero da conta que deseja consultar:\n";
             std::cin >> input;
             ss << input;
-            int numeroConta;
-            ss >> numeroConta;
+            int numero_conta = 0;
+            ss >> numero_conta;
             ss.clear();
             input.clear();
             for (auto &&i : listaContas){
-                if(i.getAccountNumber() == numeroConta)
-                    std::cout << "A conta de numero " << numeroConta << " possui o seguinte saldo: " << i.getBalance() << std::endl;
+                if(i.getAccountNumber() == numero_conta)
+                    std::cout << ">>>>>>> A conta de numero " << numero_conta << " possui o seguinte saldo: " << i.getBalance() << "\n\n";
             }
         }
         //Creditar conta
         else if(escolha == '3'){
-            std::cout << "Digite o numero da conta que deseja relizar credito e o valor (ex 666 2345.50):\n";
+            std::cout << "Digite o numero da conta que deseja relizar credito:\n";
             std::cin >> input;
+            ss  << input;
+            input.clear();
+            int numero_conta = 0;
+            ss >> numero_conta;
+
+            std::cout << "Digite o valor a ser creditado:\n";
+            std::cin >> input;
+            ss.clear();
             ss << input;
-            int numeroConta;
-            double valor;
-            ss >> numeroConta;
+            input.clear();
+            double valor = 0.0;
             ss >> valor;
+            ss.clear();
 
             for (auto &&i : listaContas){
-                if(i.getAccountNumber() == numeroConta){
-                    i.setBalance(valor); //implementar setBalance
-                    std::cout << "O valor " << valor << " foi creditado na conta de numero " << numeroConta << std::endl;
+                if(i.getAccountNumber() == numero_conta){
+                    i.setBalance(valor); 
+                    std::cout << ">>>>>>> O valor " << valor << " foi creditado na conta de numero " << numero_conta << "\n\n";
                 }
             }
             
@@ -86,18 +72,26 @@ int main(){
 
         //Debitar conta
         else if(escolha == '4'){
-            std::cout << "Digite o numero da conta que deseja relizar debito e o valor (ex 666 2345.50):\n";
+            std::cout << "Digite o numero da conta que deseja relizar o debito:\n";
             std::cin >> input;
+            ss  << input;
+            input.clear();
+            int numero_conta = 0;
+            ss >> numero_conta;
+
+            std::cout << "Digite o valor a ser debitado:\n";
+            std::cin >> input;
+            ss.clear();
             ss << input;
-            int numeroConta;
-            double valor;
-            ss >> numeroConta;
+            input.clear();
+            double valor = 0.0;
             ss >> valor;
+            ss.clear();
 
             for (auto &&i : listaContas){
-                if(i.getAccountNumber() == numeroConta){
-                    i.setBalance(valor); //implementar setBalance
-                    std::cout << "O valor " << valor << " foi debitado na conta de numero " << numeroConta << std::endl;
+                if(i.getAccountNumber() == numero_conta){
+                    i.setBalance(-valor);
+                    std::cout << ">>>>>>> O valor " << valor << " foi debitado na conta de numero " << numero_conta << "\n\n";
                 }
             }
             
@@ -105,7 +99,43 @@ int main(){
 
         //Transferência entre contas
         else if(escolha == '5'){
-            //Ler numero da conta origem-destino, valor e então executar o débito em uma e o crédito em outra
+            std::cout << "Digite o numero da conta origem:\n";
+            std::cin >> input;
+            ss << input;
+            int conta_origem = 0;
+            ss >> conta_origem;
+            input.clear();
+            ss.clear();
+
+            std::cout << "Digite o numero da conta destino:\n";
+            std::cin >> input;
+            ss << input;
+            int conta_destino = 0;
+            ss >> conta_destino;
+            input.clear();
+            ss.clear();
+
+            std::cout << "Digite o valor da transferencia:\n";
+            std::cin >> input;
+            ss << input;
+            double valor = 0.0;
+            ss >> valor;
+            input.clear();
+            ss.clear();
+
+            //Debito na conta origem
+            for (auto &&i : listaContas){
+                if(i.getAccountNumber() == conta_origem){
+                    i.setBalance(i.getBalance() - valor);
+                }
+            }
+
+            //Credito na conta destino
+            for (auto &&i : listaContas){
+                if(i.getAccountNumber() == conta_destino){
+                    i.setBalance(i.getBalance() + valor); 
+                }
+            }
         }
 
         if(escolha == '0'){
